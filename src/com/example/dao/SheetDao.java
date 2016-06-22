53ff8d7355062a9d804616308ab9177cc5974a4d
saurabh galande <saurabhgalande46@gmail.com>
2016-06-22 07:57:01 +0530

    Removed create sheet method from SheetDao

--- src/com/example/dao/SheetDao.java
+++ src/com/example/dao/SheetDao.java
@@ -4,8 +4,7 @@ import com.example.model.Sheet;
 
 public interface SheetDao {
 	
-	public void creatSheet(Sheet sheet);
-
+	
 	public void openSheet(int id);
 	
 	public Integer addSheet(Sheet sheet);
